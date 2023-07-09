772ade20476717c6a86e17b3e5e0fab247e6f60f
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-09 01:53:57 -0500

    chore: adds route for querying property with or without filters

---
+++ src/estate/routes.py
@@ -0,0 +1,16 @@
+
+
+from src.database.mysql_connection import MySQLConnection
+from src.estate import estate_routes
+from src.estate.data_access_layer.data_access_implementation import MySQLDataAccessLayer
+from src.estate.http_estate import EstateFinder
+
+
+connector = MySQLConnection()
+
+data_access_implementation = MySQLDataAccessLayer(connector)
+
+estates = EstateFinder.as_view('state finder', data_access_implementation)
+
+
+estate_routes.add_url_rule('/api/v1/estates', view_func=estates, methods=['GET'])
