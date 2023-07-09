90fbfa374f9c9f1574b69d32f94c3dddba42a399
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-09 01:53:19 -0500

    chore: adds property model

---
+++ src/estate/models/estate.py
@@ -0,0 +1,11 @@
+
+
+class Estate:
+
+    def __init__(self, address: str, city: str, status: str, price: float, description: str) -> None:
+
+        self.address = address
+        self.city = city
+        self.status = status
+        self.price = price
+        self.description = description
