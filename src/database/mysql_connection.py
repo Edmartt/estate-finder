6be34c59694345da7e38d050cf57196fe6a2b9df
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-09 01:46:34 -0500

    feat: adds mysql connection

---
+++ src/database/mysql_connection.py
@@ -0,0 +1,37 @@
+import os
+import mysql.connector
+
+from src.database.database_interface import IDatabaseConnecton
+
+
+class MySQLConnection(IDatabaseConnecton):
+
+    def __init__(self) -> None:
+
+        self.host = os.environ.get('MYSQL_HOST')
+        self.port = os.environ.get('MYSQL_PORT')
+        self.db = os.environ.get('MYSQL_DB')
+        self.user = os.environ.get('MYSQL_USER')
+        self.password = os.environ.get('MYSQL_PASSWORD')
+
+    def get_connection(self) -> tuple:
+
+        connection = mysql.connector.connect(
+            host=self.host,
+            database=self.db,
+            user=self.user,
+            password=self.password,
+            port=self.port
+        )
+
+        cursor = connection.cursor(dictionary=True)
+        return connection, cursor
+
+    def close_connection(self, e=None):
+
+        connection, cursor = self.get_connection()
+
+        if connection is not None:
+
+            connection.close()
+    
