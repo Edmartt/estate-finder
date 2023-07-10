262ead65c81c2fc3f1b2f5705725f25acac90d03
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-10 00:45:01 -0500

    chore: adds check for data type in case of dict checks a key when a filter is not valid

--- src/estate/http_estate.py
+++ src/estate/http_estate.py
@@ -4,16 +4,16 @@ from flask import jsonify, request
 from flask.views import MethodView
 
 from src.estate.data_access_layer.data_access_interface import DataAccessInterface
+from src.estate.utils.filter_validator import validate_http_filters
+from src.estate.estate_errors.errors import bad_request, not_found
 
 
 class EstateFinder(MethodView):
 
     def __init__(self, data_access: DataAccessInterface) -> None:
-        
         self.data_access = data_access
 
     def get(self):
-
         filters = request.args
 
         if len(list(filters)) == 0:
@@ -21,10 +21,16 @@ class EstateFinder(MethodView):
 
             return jsonify({'response': result}), 200
 
-        year = filters.get('year')
-        city = filters.get('city')
-        status = filters.get('status')
-        args = (year, city, status)
+        valid_filters = validate_http_filters(filters)
+
+        if type(valid_filters) is dict:
+
+            if valid_filters.get('not_valid'):
+                not_valid_filter = valid_filters.get('not_valid')
+
+                return jsonify({'response': f'some filters are not valid: {not_valid_filter}'}), 400
+
+        args = valid_filters
 
         result = self.data_access.read_properties_with_filters(*args)
 
