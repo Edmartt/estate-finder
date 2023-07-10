caa3cd5d57d8b17cf4c72647c2b50aa3139573b0
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-10 00:34:48 -0500

    feat: adds swagger support

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,29 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+
 from config import config
 
 
 def create_app(config_name: str) -> Flask:
 
     app = Flask(__name__)
+    SWAGGER_URL = '/api/docs'
+    API_URL = '/static/swagger.yaml'
+
+    swagger_blueprint = get_swaggerui_blueprint(
+            SWAGGER_URL,
+            API_URL,
+            config={
+                'estate finder': 'search estates'
+                }
+            )
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     from src.estate import estate_routes
+
     app.register_blueprint(estate_routes)
+    app.register_blueprint((swagger_blueprint))
 
     return app
 
